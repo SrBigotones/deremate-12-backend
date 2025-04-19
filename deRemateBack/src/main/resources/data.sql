48b2c8bb2182db05d4abc1e967560a314752f5ba
Augusto Gangui <61172521+SrBigotones@users.noreply.github.com>
2025-04-19 18:47:35 -0300

    sample data

--- deRemateBack/src/main/resources/data.sql
+++ deRemateBack/src/main/resources/data.sql
@@ -3,3 +3,9 @@ INSERT INTO USUARIO(USERNAME, PASSWORD, EMAIL, NOMBRE, APELLIDO, DOCUMENTO) VALU
    ('user2', '$2a$10$bhoNyi0sxaGQu.50zOiUL.SKpSPGHytwGruHhYYP2hSDZpkYHEIri', 'user2@test.com', 'Usuario', 'Dos', 34444444),
    ('user3', '$2a$10$bhoNyi0sxaGQu.50zOiUL.SKpSPGHytwGruHhYYP2hSDZpkYHEIri', 'user3@test.com', 'Usuario', 'Tres', 35555555),
    ('user4', '$2a$10$bhoNyi0sxaGQu.50zOiUL.SKpSPGHytwGruHhYYP2hSDZpkYHEIri', 'user4@test.com', 'Usuario', 'Cuatro', 36666666);
+
+
+INSERT INTO ENTREGA(ESTADO, FECHA_CREACION, FECHA_ENTREGA, USUARIO_ID, DIRECCION_ENTREGA, OBSERVACIONES) VALUES
+    (1, '2025-04-19 14:23:45.123456', '2025-04-19 14:23:45.123456', 1, 'Falsa 123', ''),
+    (1, '2025-04-19 14:23:45.123456', '2025-04-19 14:23:45.123456', 2, 'Falsa 123', ''),
+    (2, '2025-04-20 14:23:45.123456', '2025-04-19 14:23:45.123456', 1, 'Falsa 123', '');
